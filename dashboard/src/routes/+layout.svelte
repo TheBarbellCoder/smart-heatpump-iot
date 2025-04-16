726b82107680d27002d41d57ccca549193c393eb
Avinash Chikkadlur Ravi Shankar <avinash@thebarbellcoder.com>
2025-04-16 15:05:06 +0200

    #6 built navbar with shadcn-svelte and phoshor-svelte
    closes #6

--- dashboard/src/routes/+layout.svelte
+++ dashboard/src/routes/+layout.svelte
@@ -1,7 +1,27 @@
-<script lang="ts">
+<script>
 	import '../app.css';
-
+	import IconContext from "phosphor-svelte/lib/IconContext";
+	import Thermometer from "phosphor-svelte/lib/Thermometer";
+	import { Button } from "$lib/components/ui/button";
+	import { Switch } from "$lib/components/ui/switch";
+	import { Label } from "$lib/components/ui/label";
+	let isChecked = $state(false);
+	let isSimulated = $state(false);
 	let { children } = $props();
 </script>
+
+<div class="sticky flex justify-between items-center p-2 border-b box-border shadow-sx">
+	<div class="flex items-center gap-4">
+		<Thermometer weight="fill" size="{32}" />
+		<div class="text-lg font-semibold">Heatpump {isSimulated? 'Simulator': 'Dashboard'}</div>
+	</div>
+	<div class="flex items-center gap-y-4 gap-x-6">
+		<div class="flex items-center gap-x-2">
+			<Switch bind:checked={isChecked}/>
+			<Label for='system-state'>{isChecked? 'System On': 'System Off'}</Label>
+		</div>
+		<Button disabled={!isSimulated}>Restart Simulation</Button>
+	</div>
+</div>
 
 {@render children()}
