4e43c1242f4ae6179de4bd050b7053e654f72b33
Avinash Chikkadlur Ravi Shankar <avinash@thebarbellcoder.com>
2025-04-21 03:45:48 +0200

    #7 added badge and card ui components for system overview section
    * Added Badge and Card shadcn-svelte UI components to the project
    * Created a basic outline for the "System Overview" section of the dashboard

--- dashboard/src/routes/+layout.svelte
+++ dashboard/src/routes/+layout.svelte
@@ -1,26 +1,35 @@
 <script>
 	import '../app.css';
+	import { setContext } from "svelte";
 	import IconContext from "phosphor-svelte/lib/IconContext";
 	import Thermometer from "phosphor-svelte/lib/Thermometer";
 	import { Button } from "$lib/components/ui/button";
 	import { Switch } from "$lib/components/ui/switch";
 	import { Label } from "$lib/components/ui/label";
+
 	let isChecked = $state(false);
 	let isSimulated = $state(false);
+
+	setContext("systemState", {
+		get: () => isChecked,
+		set: (value) => isChecked = value,
+		toggle: () => isChecked = !isChecked
+	});
+
 	let { children } = $props();
 </script>
 
 <div class="sticky flex justify-between items-center p-2 border-b box-border shadow-sx">
 	<div class="flex items-center gap-4">
-		<Thermometer weight="fill" size="{32}" />
-		<div class="text-lg font-semibold">Heatpump {isSimulated? 'Simulator': 'Dashboard'}</div>
+		<Thermometer weight="fill" size=32 />
+		<div class="text-lg font-medium">Heatpump { isSimulated ? "Simulator": "Dashboard" }</div>
 	</div>
 	<div class="flex items-center gap-y-4 gap-x-6">
 		<div class="flex items-center gap-x-2">
-			<Switch bind:checked={isChecked}/>
-			<Label for='system-state'>{isChecked? 'System On': 'System Off'}</Label>
+			<Switch bind:checked={ isChecked }/>
+			<Label for='system-state'>{ isChecked ? "System On": "System Off" }</Label>
 		</div>
-		<Button disabled={!isSimulated}>Restart Simulation</Button>
+		<Button variant="outline" disabled={ !isSimulated }>Restart Simulation</Button>
 	</div>
 </div>
 
--- dashboard/src/routes/+page.svelte
+++ dashboard/src/routes/+page.svelte
@@ -1,2 +1,65 @@
+<script>
+  import { getContext } from "svelte";
+  import * as Card from "$lib/components/ui/card";
+  import { Badge } from "$lib/components/ui/badge";
+  import { ThermometerSimple, Fire, Snowflake, Lightning, Park } from "phosphor-svelte"
+
+  const systemState = getContext("systemState");
+  const opModes = {op1: "Heating", op2: "Cooling"};
+  let currentMode = "op1";
+
+  // $effect(()=>{console.log(systemState.get());});
+</script>
+
+<Card.Root class="flex-row p-2 m-6 shadow-xs">
+  <Card.Header class="flex-row justify-between w-full items-center p-2 box-border">
+    <div class="flex-row">
+      <Card.Title class="text-xl font-medium">System Overview</Card.Title>
+      <Card.Description class="text-base">Current heatpump perfomance and status</Card.Description>
+    </div>
+    <div class="flex gap-x-2 box-border">
+      <Badge variant={ systemState.get() ? "default" :"secondary" } class="text-sm font-medium">{ systemState.get() ? "Running" :"Offline" }</Badge>
+      <Badge variant="outline" class="flex gap-x-1 text-sm font-medium">
+        {#if opModes[currentMode] === "Heating"}
+          <Fire color="#aa201f" weight="duotone" size=20 />
+        {:else}
+          <Snowflake color="#9a9996" weight="duotone" size=20 />
+        {/if}
+        {opModes[currentMode]}</Badge>
+    </div>
+  </Card.Header>
+  <Card.Content class="flex gap-x-6">
+    <Card.Root class="w-full shadow-2xs">
+      <Card.Header>
+        <div class="flex gap-x-1 items-center">
+          <ThermometerSimple color="#0b6c89" weight="duotone" size=24 />
+          <Card.Title class="text-base font-normal">Indoor Temperature</Card.Title>
+        </div>
+        <Card.Description></Card.Description> 
+      </Card.Header>
+    </Card.Root>
+    <Card.Root class="w-full shadow-2xs">
+      <Card.Header>
+        <div class="flex gap-x-1 items-center">
+          <Park color="#ba6e13" weight="duotone" size=24 />
+          <Card.Title class="text-base font-normal">Outdoor Temperature</Card.Title>
+        </div>
+        <Card.Description></Card.Description> 
+      </Card.Header>
+    </Card.Root>
+    <Card.Root class="w-full shadow-2xs">
+      <Card.Header>
+        <div class="flex gap-x-1 items-center">
+          <Lightning color="#5515c9" weight="duotone" size=24 />
+          <Card.Title class="text-base font-normal">Energy Consumption</Card.Title>
+        </div>
+        <Card.Description></Card.Description> 
+      </Card.Header>
+    </Card.Root>
+
+  </Card.Content>
+  
+</Card.Root>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
