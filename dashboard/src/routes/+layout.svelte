297397e5425f4e37b9411de2724f4420db0c31a1
Avinash Chikkadlur Ravi Shankar <avinash@thebarbellcoder.com>
2025-05-30 07:20:09 +0200

    #8 replaced node.js with deno and shadcn-svelete with bits-ui
    Replacing node.js with deno cleaned up a lot of unnecessary packages and config files.
    
    Replaced shadcn-svelte with bits-ui, a svelte native UI library for finer configurarion.
    This helped remove the <div> in <p> error in shadcn-svelte Card component.

--- dashboard/src/routes/+layout.svelte
+++ dashboard/src/routes/+layout.svelte
@@ -1,11 +1,10 @@
 <script>
   import "../app.css";
+
   import { setContext } from "svelte";
   import IconContext from "phosphor-svelte/lib/IconContext";
-  import Thermometer from "phosphor-svelte/lib/Thermometer";
-  import { Button } from "$lib/components/ui/button";
-  import { Switch } from "$lib/components/ui/switch";
-  import { Label } from "$lib/components/ui/label";
+  import { Thermometer } from "phosphor-svelte";
+  import { Button, Label, Switch } from "bits-ui";
 
   let isChecked = $state(false);
   let isSimulated = $state(true);
@@ -23,20 +22,51 @@
   let { children } = $props();
 </script>
 
-<div class="sticky flex justify-between items-center p-2 border-b box-border shadow-sx">
+<div class="sticky flex justify-between items-center p-3 border-b box-border shadow-sx">
   <div class="flex items-center gap-4">
     <Thermometer weight="fill" size="32" />
-    <div class="text-lg font-medium">
+    <div class="text-xl tracking-tight font-bold">
       Heatpump {isSimulated ? "Simulator" : "Dashboard"}
     </div>
   </div>
   <div class="flex items-center gap-y-4 gap-x-6">
     <div class="flex items-center gap-x-2">
-      <Switch bind:checked={isChecked} />
-      <Label for="system-state">{isChecked ? "System On" : "System Off"}</Label>
+      <Switch.Root
+        id="on-off"
+        bind:checked={isChecked}
+        class="
+          focus-visible:ring-foreground focus-visible:ring-offset-background
+          data-[state=checked]:bg-foreground data-[state=unchecked]:bg-dark-10
+          data-[state=unchecked]:shadow-mini-inset dark:data-[state=checked]:bg-foreground
+          focus-visible:outline-hidden peer inline-flex h-[31px] min-h-[31px] w-[54px]
+          shrink-0 cursor-pointer items-center rounded-full px-[3px] transition-colors
+          focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed
+          disabled:opacity-50
+        "
+      >
+        <Switch.Thumb
+          class="
+            bg-background data-[state=unchecked]:shadow-mini dark:border-background/30
+            dark:bg-foreground dark:shadow-popover pointer-events-none block size-[24px]
+            shrink-0 rounded-full transition-transform data-[state=checked]:translate-x-6
+            data-[state=unchecked]:translate-x-0 dark:border
+            dark:data-[state=unchecked]:border
+          "
+        />
+      </Switch.Root>
+      <Label.Root for="on-off" class="text-sm font-medium">{
+        isChecked ? "System On" : "System Off"
+      }</Label.Root>
     </div>
-    <Button variant="outline" disabled={!isSimulated}
-    >Restart Simulation</Button>
+    <Button.Root
+      class="
+        rounded-input bg-dark text-background shadow-mini hover:bg-dark/95 inline-flex
+        h-12 items-center justify-center px-[21px] text-[15px] font-semibold
+        active:scale-[0.98] active:transition-all disabled:bg-muted
+        disabled:text-muted-foreground
+      "
+      disabled={!isSimulated}
+    >Restart Simulation</Button.Root>
   </div>
 </div>
 
--- dashboard/src/routes/+page.svelte
+++ dashboard/src/routes/+page.svelte
@@ -1,136 +1,208 @@
 <script>
   import { getContext } from "svelte";
   import { AreaChart } from "layerchart";
-  import * as Card from "$lib/components/ui/card";
-  import { Badge } from "$lib/components/ui/badge";
-  import { Slider } from "$lib/components/ui/slider";
-  import { ThermometerSimple, Fire, Snowflake, Fan, Lightning, Park, CaretDown, CaretUp, Minus } from "phosphor-svelte"
+  import { Slider } from "bits-ui";
+  import cn from "clsx";
+  import {
+    CaretDown,
+    CaretUp,
+    Fan,
+    Fire,
+    Lightning,
+    Minus,
+    Park,
+    Snowflake,
+    ThermometerSimple,
+  } from "phosphor-svelte";
 
   const systemState = getContext("systemState");
 
-  let targetTemp = $state(19);  // °C
+  let targetTemp = $state(19); // °C
   let currentTemp = $state(19); // °C
   let outdoorTemp = $state(21); // °C
-  // svelte-ignore state_referenced_locally
-  // Make deltaTemp a $devrived() state
   let deltaTemp = $derived(targetTemp - currentTemp);
 
-  const opModes = {op1: "Heating", op2: "Cooling", op3: "Fan"};
-  let currentMode = $derived(outdoorTemp < targetTemp ? "op1" : outdoorTemp > targetTemp ? "op2" : "op3");
+  const opModes = { op1: "Heating", op2: "Cooling", op3: "Fan" };
+  let currentMode = $derived(
+    outdoorTemp < targetTemp
+      ? "op1"
+      : outdoorTemp > targetTemp
+      ? "op2"
+      : "op3",
+  );
 
   let cop = 0.1;
   let currentEnergyConsumed = 1; // kW
-
 </script>
 
-<Card.Root class="flex-row p-2 m-6 shadow-xs" disabled={ !systemState.getOnOffState() }>
-  <Card.Header class="flex-row justify-between w-full items-center p-2 box-border">
-    <div class="flex-row">
-      <Card.Title class="text-xl font-medium">System Overview</Card.Title>
-      <Card.Description class="text-base">Current heatpump perfomance and status</Card.Description>
-    </div>
-    <div class="flex gap-x-2 box-border">
-      <Badge variant={ systemState.getOnOffState() ? "default" :"secondary" } class="text-sm font-medium">{ systemState.getOnOffState() ? "Running" :"Offline" }</Badge>
-      <Badge variant="outline" class="flex gap-x-1 text-sm font-medium">
+<!-- System Overview section -->
+<div class="flex-row p-3 m-6 border rounded-card-sm box-border items-center">
+  <div class="flex justify-between">
+    <div class="text-lg font-semibold tracking-tight">System Overview</div>
+    <div class="flex gap-x-3 box-border justify-center items-center">
+      <div
+        class="
+          text-sm font-semibold border box-border px-3 py-1 rounded-15px
+          { systemState.getOnOffState()? 'bg-dark text-background' :
+          'bg-muted text-muted-foreground border-muted-foreground' }
+        "
+      >
+        {systemState.getOnOffState() ? "Running" : "Offline"}
+      </div>
+      <div class="flex gap-x-1 text-sm font-semibold box-border border border-muted-foreground rounded-15px px-3 py-1">
         {#if opModes[currentMode] === "Heating"}
-          <Fire color="#aa201f" weight="duotone" size=20 />
+          <Fire weight="fill" size="20" />
         {:else if opModes[currentMode] === "Cooling"}
-          <Snowflake class="text-muted-foreground" weight="duotone" size=20 />
+          <Snowflake weight="bold" size="20" />
         {:else}
-          <Fan class="text-muted-foreground" weight="fill" size=20 />
+          <Fan weight="fill" size="20" />
         {/if}
-        { opModes[currentMode] }</Badge>
+        {opModes[currentMode]}
+      </div>
     </div>
-  </Card.Header>
-  <Card.Content class="flex gap-x-6">
-    <Card.Root class="w-full shadow-2xs">
-      <Card.Header>
-        <div class="flex gap-x-1 items-center">
-          <ThermometerSimple color="#0b6c89" weight="duotone" size=24 />
-          <Card.Title class="text-base font-normal">Indoor Temperature</Card.Title>
-        </div>
-        <Card.Description>
-          <span>
-            <span class="flex items-baseline p-2 gap-x-1">
-              <span class="text-5xl font-medium text-primary">{ currentTemp }</span>
-              <span class="text-lg text-primary font-light">&deg;C</span>
-            </span>
-            <span class="flex px-2 gap-x-1 items-center">
-            {#if deltaTemp > 0}
-              <CaretUp color="#aa210f" weight="bold" />
-              <span>{ Math.abs(deltaTemp) }&deg;C to target</span>
-            {:else if deltaTemp < 0}
-              <CaretDown color="#aa210f" weight="bold" />
-              <span>{ Math.abs(deltaTemp) }&deg;C from target</span>
-            {:else}
-              <Minus color="#0b6c89" weight="bold" />
-              <span>At target temperature</span>
-            {/if}
-          </span>
-        </Card.Description> 
-      </Card.Header>
-    </Card.Root>
-    <Card.Root class="w-full shadow-2xs">
-      <Card.Header>
-        <div class="flex gap-x-1 items-center">
-          <Park color="#ba6e13" weight="duotone" size=24 />
-          <Card.Title class="text-base font-normal">Outdoor Temperature</Card.Title>
-        </div>
-        <Card.Description>
-          <span>
-            <span class="flex items-baseline p-2 gap-x-1">
-              <span class="text-5xl font-medium text-primary">{ outdoorTemp }</span>
-              <span class="text-lg text-primary font-light">&deg;C</span>
-            </span>
-            <span>{ systemState.getSimulationState() ? "Simulated environment": "Current temperature" }</span>
-          </span>
-        </Card.Description> 
-      </Card.Header>
-    </Card.Root>
-    <Card.Root class="w-full shadow-2xs">
-      <Card.Header>
-        <div class="flex gap-x-1 items-center">
-          <Lightning color="#5515c9" weight="duotone" size=24 />
-          <Card.Title class="text-base font-normal">Energy Consumption</Card.Title>
-        </div>
-        <Card.Description>
-          <span>
-            <span class="flex items-baseline p-2 gap-x-1">
-              <span class="text-5xl font-medium text-primary">{ currentEnergyConsumed }</span>
-              <span class="text-lg text-primary font-light">kW</span>
-            </span>
-            <span class="px-2">COP: { cop }</span>
-          </span>
-        </Card.Description> 
-      </Card.Header>
-    </Card.Root>
-  </Card.Content>
-</Card.Root>
+  </div>
+  <div class="text-sm text-foreground/70 font-semibold">
+    Current heatpump performance and status
+  </div>
 
-<Card.Root class="flex-row p-2 m-6 shadow-xs" disabled={ !systemState.getOnOffState() }>
-  <Card.Header class="flex-row justify-between w-full items-center p-2 box-border">
-    <Card.Title class="text-xl font-medium">Temperature Control</Card.Title>
-  </Card.Header>
-  <Card.Description>
-    <div class="flex-row px-2 w-1/4">
-      <span class="flex justify-between items-center {systemState.getSimulationState()?'':'hidden'}">
-        <span class="text-primary text-base">Outdoor Temperature: { outdoorTemp }&deg;C</span>
-        <span class="flex gap-x-2">
-          <Snowflake class="text-muted-foreground" weight="duotone" size=18 />
-          <Slider value={ [outdoorTemp] } onValueChange={ (values) => outdoorTemp = values[0] } min={-20} max={42} step={0.5} class="w-xs" />
-          <Fire color="#aa210f" weight="duotone" size=18 />
-        </span>
-      </span>
-      <span class="flex justify-between items-center">
-        <span class="text-primary text-base">Indoor Temperature: { targetTemp }&deg;C</span>
-        <span class="flex gap-x-2">
-          <Snowflake class="text-muted-foreground" weight="duotone" size=18 />
-          <Slider value={ [targetTemp] } onValueChange={ (values) => targetTemp = values[0] } min={18} max={22} step={0.5} class="w-xs"/>
-          <Fire color="#aa210f" weight="duotone" size=18 />
-        </span>
-      </span>
+  <!-- Cards -->
+  <div class="flex m-6 gap-6 justify-between items-center">
+    <!-- Indoor Temperature Card -->
+    <div class="flex-row w-full p-3 box-border border rounded-card-sm">
+      <div class="flex gap-x-1 items-center">
+        <ThermometerSimple weight="regular" size="24" />
+        <div class="font-medium">Indoor Temperature</div>
+      </div>
+      <div class="flex items-baseline p-3 gap-x-1">
+        <span class="text-6xl font-medium text-primary">{targetTemp}</span>
+        <span class="text-lg text-primary font-light">&deg;C</span>
+      </div>
+      <div class="flex px-3 gap-x-1 items-center">
+        {#if deltaTemp > 0}
+          <CaretUp weight="bold" />
+          <span class="text-sm text-foreground/70">{Math.abs(deltaTemp)}&deg;C
+            to target</span>
+        {:else if deltaTemp < 0}
+          <CaretDown weight="bold" />
+          <span class="text-sm text-foreground/70">{Math.abs(deltaTemp)}&deg;C
+            from target</span>
+        {:else}
+          <Minus weight="bold" />
+          <span class="text-sm text-foreground/70">At target temperature</span>
+        {/if}
+      </div>
     </div>
-  </Card.Description>
-</Card.Root>
+
+    <!-- Outdoor Temperature Card -->
+    <div class="flex-row w-full p-3 box-border border rounded-card-sm">
+      <div class="flex gap-x-1 items-center">
+        <Park weight="regular" size="24" />
+        <div class="font-medium">Outdoor Temperature</div>
+      </div>
+      <div class="flex items-baseline p-3 gap-x-1">
+        <span class="text-6xl font-medium text-primary">{outdoorTemp}</span>
+        <span class="text-lg text-primary font-light">&deg;C</span>
+      </div>
+      <div class="flex px-3 gap-x-1 items-center">
+        <div Class="text-sm text-foreground/70">
+          {
+            systemState.getSimulationState()
+            ? "Simulated environment"
+            : "Current temperature"
+          }
+        </div>
+      </div>
+    </div>
+
+    <!-- Energy Consumption Card -->
+    <div class="flex-row w-full p-3 box-border border rounded-card-sm">
+      <div class="flex gap-x-1 items-center">
+        <Lightning weight="regular" size="24" />
+        <div class="font-medium">Energy Consumption</div>
+      </div>
+      <div class="flex items-baseline p-3 gap-x-1">
+        <span class="text-6xl font-medium text-primary">{
+          currentEnergyConsumed
+        }</span>
+        <span class="text-lg text-primary font-light">kW</span>
+      </div>
+      <div class="flex px-3 gap-x-1 items-center">
+        <div Class="text-sm text-foreground/70">
+          COP: {cop}
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Temperature Control section -->
+<div class="flex-row p-3 m-6 border rounded-card-sm box-border items-center">
+  <div class="flex justify-between">
+    <div class="text-lg font-semibold tracking-tight">Temperature Control</div>
+    <div class="flex-row box-border justify-center items-center">
+      <!-- Slider for outdoor temperature setting -->
+      <div class="flex gap-x-3 h-7 justify-between items-center">
+        <div class="text-sm text-foreground/70 font-semibold w-[210px]">
+          Outdoor Temperature: {outdoorTemp}&deg;C
+        </div>
+        <Snowflake weight="bold" size="20" />
+        <Slider.Root
+          type="single"
+          bind:value={outdoorTemp}
+          min={-20}
+          max={42}
+          step={0.5}
+          class="relative flex w-[200px] touch-none select-none items-center"
+        >
+          <span
+            class="bg-dark-10 relative h-2 w-[200px] cursor-pointer overflow-hidden rounded-full"
+          >
+            <Slider.Range class="bg-foreground absolute h-full" />
+          </span>
+          <Slider.Thumb
+            index={0}
+            class={cn(
+              "border-border-input bg-background hover:border-dark-40 focus-visible:ring-foreground dark:bg-foreground dark:shadow-card focus-visible:outline-hidden data-active:scale-[0.98] data-active:border-dark-40 block size-[23px] cursor-pointer rounded-full border shadow-sm transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+            )}
+          />
+        </Slider.Root>
+        <Fire weight="bold" size="20" />
+      </div>
+
+      <!-- Slider for indoor temperature setting -->
+      <div class="flex gap-x-3 h-7 justify-between items-center">
+        <div class="text-sm text-foreground/70 font-semibold w-[210px]">
+          Indoor Temperature: {targetTemp}&deg;C
+        </div>
+        <Snowflake weight="bold" size="20" />
+        <Slider.Root
+          type="single"
+          bind:value={targetTemp}
+          min={18}
+          max={22}
+          step={0.5}
+          class="relative flex w-[200px] touch-none select-none items-center"
+        >
+          <span
+            class="bg-dark-10 relative h-2 w-[200px] cursor-pointer overflow-hidden rounded-full"
+          >
+            <Slider.Range class="bg-foreground absolute h-full" />
+          </span>
+          <Slider.Thumb
+            index={0}
+            class={cn(
+              "border-border-input bg-background hover:border-dark-40 focus-visible:ring-foreground dark:bg-foreground dark:shadow-card focus-visible:outline-hidden data-active:scale-[0.98] data-active:border-dark-40 block size-[23px] cursor-pointer rounded-full border shadow-sm transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+            )}
+          />
+        </Slider.Root>
+        <Fire weight="bold" size="20" />
+      </div>
+    </div>
+  </div>
+</div>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read
+  the documentation
+</p>
